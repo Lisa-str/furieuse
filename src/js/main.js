e09a76c0cf4d6427fb95a3f106398226d1f7f58e
Lisa-str <Lisa.Staruch@studentfr.ch>
2023-11-14 15:46:33 +0100

    base with cubes

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".slider-track", {
+  scrollTrigger: {
+    trigger: ".scroll-horizontal",
+    start: "top top",
+    end: "+=300%",
+    pin: true,
+    scrub: true,
+    markers: true,
+  },
+  x: "-75%",
+  ease: "sine.inOut",
+});
