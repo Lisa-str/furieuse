c00302169de08bb49e3e87b80797d5880ce8c5b1
Lisa-str <Lisa.Staruch@studentfr.ch>
2023-11-15 11:29:56 +0100

    scroll-horizontal to the end js

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,30 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".slider-track", {
+let container = document.querySelector(".slider-track");
+
+// l'animation GSAP permettant de scroller
+let slider = gsap.to(container, {
+  x: () =>
+    -(container.scrollWidth - document.documentElement.clientWidth) -
+    parseInt(window.getComputedStyle(container).marginLeft) -
+    parseInt(window.getComputedStyle(container).marginRight) +
+    "px",
+  ease: "power1.out",
   scrollTrigger: {
     trigger: ".scroll-horizontal",
     start: "top top",
-    end: "+=300%",
-    pin: true,
-    scrub: true,
+    end: () => "+=" + container.clientWidth,
     // markers: true,
+    scrub: true,
+    pin: true,
   },
-  x: "-75%",
-  ease: "sine.inOut",
+});
+
+// le code suivant écoute le redimensionnement de la fenêtre et actualise le slider
+window.addEventListener("resize", () => {
+  let progress = slider.totalProgress();
+  slider.progress(0);
+  slider.invalidate();
+  slider.totalProgress(progress);
 });
